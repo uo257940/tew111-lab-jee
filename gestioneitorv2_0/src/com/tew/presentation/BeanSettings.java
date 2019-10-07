b5e7595179a5c6ec340da424c6faece05af72b13
uo257940 <uo257940@uniovi.es>
2019-10-07 12:03:55 +0200

    TEW –4.5 Inyección Dependencia Bean Alumno

--- gestioneitorv2_0/src/com/tew/presentation/BeanSettings.java
+++ gestioneitorv2_0/src/com/tew/presentation/BeanSettings.java
@@ -38,41 +38,41 @@ partida “es”*/
 		}
 	}
 
-public void setEnglish(ActionEvent event) {
-	locale = ENGLISH;
-	try {
-		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
-		if (alumno != null)
-			if (alumno.getId()== null) //valores por defecto del alumno, si no NO inicializar
-				alumno.iniciaAlumno(null);
-	} catch (Exception ex){
-		ex.printStackTrace();
-	}}
+	public void setEnglish(ActionEvent event) {
+		locale = ENGLISH;
+		try {
+			FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+			if (alumno != null)
+				if (alumno.getId()== null) //valores por defecto del alumno, si no NO inicializar
+					alumno.iniciaAlumno(null);
+		} catch (Exception ex){
+			ex.printStackTrace();
+		}}
 
 
-//Se inicia correctamente el Managed Bean inyectado si JSF lo hubiera creado
-//y en caso contrario se crea.
-//(hay que tener en cuenta que es un Bean de sesión)
-//Se usa @PostConstruct, ya que en el contructor no se sabe todavía si
-//el MBean ya estaba construido y en @PostConstruct SI.
-@PostConstruct
-public void init() {
-	System.out.println("BeanSettings - PostConstruct");
-	//Buscamos el alumno en la sesión. Esto es un patrón factoría claramente.
-	alumno =
-			(BeanAlumno)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(new String("alumno"));
-	//si no existe lo creamos e inicializamos
-	if (alumno == null) {
-		System.out.println("BeanSettings - No existia");
-		alumno = new BeanAlumno();
-		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "alumno", alumno);
+	//Se inicia correctamente el Managed Bean inyectado si JSF lo hubiera creado
+	//y en caso contrario se crea.
+	//(hay que tener en cuenta que es un Bean de sesión)
+	//Se usa @PostConstruct, ya que en el contructor no se sabe todavía si
+	//el MBean ya estaba construido y en @PostConstruct SI.
+	@PostConstruct
+	public void init() {
+		System.out.println("BeanSettings - PostConstruct");
+		//Buscamos el alumno en la sesión. Esto es un patrón factoría claramente.
+		alumno =
+				(BeanAlumno)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get(new String("alumno"));
+		//si no existe lo creamos e inicializamos
+		if (alumno == null) {
+			System.out.println("BeanSettings - No existia");
+			alumno = new BeanAlumno();
+			FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put( "alumno", alumno);
+		}
 	}
-}
-//Es sólo a modo de traza.
-@PreDestroy
-public void end()
-{
-	System.out.println("BeanSettings - PreDestroy");
-}
+	//Es sólo a modo de traza.
+	@PreDestroy
+	public void end()
+	{
+		System.out.println("BeanSettings - PreDestroy");
+	}
 }
 
