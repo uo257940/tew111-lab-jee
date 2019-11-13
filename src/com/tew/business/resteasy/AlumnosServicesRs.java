2ec1ee25b222902231ce8ade4f494f2ed010c868
UO257940 <UO257940@GI-AN-B6_06.aulasuo.uniovi.es>
2019-11-13 16:53:47 +0100

    TEW – 7.3 Servicios – Implementación de servicio

--- src/com/tew/business/resteasy/AlumnosServicesRs.java
+++ src/com/tew/business/resteasy/AlumnosServicesRs.java
@@ -5,10 +5,11 @@ import com.tew.business.AlumnosService;
 import com.tew.business.exception.EntityAlreadyExistsException;
 import com.tew.business.exception.EntityNotFoundException;
 import com.tew.model.Alumno;
+
 @Path("/AlumnosServicesRs")
 public interface AlumnosServicesRs extends AlumnosService{
 	@GET @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-	public List<Alumno> getAlumnos();
+	public List<Alumno> getAlumnos() throws Exception ;
 	@GET
 	@Path("{id}") @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	Alumno findById(@PathParam("id") Long id) throws EntityNotFoundException;
