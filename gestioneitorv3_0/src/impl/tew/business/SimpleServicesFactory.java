12514ff3d262caca3cbce13492fc0a85da5282e9
u257940 <uo257940@uniovi.es>
2019-11-20 14:25:25 +0100

    Prueba

---
+++ gestioneitorv3_0/src/impl/tew/business/SimpleServicesFactory.java
@@ -0,0 +1,21 @@
+package impl.tew.business;
+
+
+import com.tew.business.AlumnosService;
+import com.tew.business.LoginService;
+import com.tew.business.ServicesFactory;
+
+public class SimpleServicesFactory implements ServicesFactory {
+
+	@Override
+	public AlumnosService createAlumnosService() {
+		return new SimpleAlumnosService();
+	}
+	
+	@Override
+	public LoginService createLoginService() {
+		return new SimpleLoginService();
+	}
+	
+
+}
